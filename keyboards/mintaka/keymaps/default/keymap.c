fbedfcd6550caaab5964be6fac7bd2a027f0d25c
shoenot <mail@shurjo.me>
2025-02-27 01:12:16 +0600

    quantum display

--- keyboards/mintaka/keymaps/default/keymap.c
+++ keyboards/mintaka/keymaps/default/keymap.c
@@ -15,10 +15,14 @@
 #define OLED_I2C_ADDRESS 0x3c
 #define SCREEN_NUM_LINES 4
 #define SCREEN_NUM_CHARS 19
-#define SCREEN_BUFFER_LENGTH (SCREEN_NUM_CHARS * SCREEN_NUM_LINES + 1)
+#define NUM_SCREENS 4
+#define SCREEN_BUFFER_LENGTH (SCREEN_NUM_CHARS * SCREEN_NUM_LINES * NUM_SCREENS + 1)
+#define SCREEN_TIMEOUT 60000
 
 enum custom_keycodes {
-    KC_P00 = SAFE_RANGE
+    KC_P00 = SAFE_RANGE,
+    ENC_INC,
+    ENC_DEC
 };
 
 // Headers for raw hid
@@ -39,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_BASE_LAYER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) }
+    [_BASE_LAYER] = { ENCODER_CCW_CW(ENC_INC, ENC_DEC), ENCODER_CCW_CW(MS_WHLU, MS_WHLD) }
 };
 
 // HID Communication
@@ -48,10 +52,13 @@ char screen_data_buffer[SCREEN_BUFFER_LENGTH - 1] = {0};
 char temp_data_buffer[SCREEN_BUFFER_LENGTH - 1] = {0};
 bool display_updated = false;
 int current_line_number = 0;
+uint8_t current_data_screen = 0;
+
 painter_device_t oled_display;
 painter_font_handle_t display_font;
 painter_font_handle_t display_font_big;
 painter_image_handle_t boot_splash;
+uint32_t screen_timer;
 
 void text_to_buffer(char *text, char *buffer, int length, int line_number) {
     memset(&buffer[line_number * SCREEN_NUM_CHARS], 0, SCREEN_NUM_CHARS);
@@ -60,18 +67,6 @@ void text_to_buffer(char *text, char *buffer, int length, int line_number) {
 
 void clear_buffer(char *buffer) {
     memset(&buffer, 0, sizeof(buffer));
-}
-
-void raw_hid_receive(uint8_t *data, uint8_t length) {
-    text_to_buffer((char *)data, temp_data_buffer, SCREEN_NUM_CHARS, current_line_number);
-    current_line_number++;
-    if (current_line_number > 3) {
-        clear_buffer(screen_data_buffer);
-        memcpy(screen_data_buffer, temp_data_buffer, sizeof(temp_data_buffer));
-        current_line_number = 0;
-        clear_buffer(temp_data_buffer);
-        display_updated = true;
-    }
 }
 
 void keyboard_post_init_kb(void) {
@@ -85,10 +80,11 @@ void keyboard_post_init_kb(void) {
 }
 
 
-void display_write(void) {
+void display_write(int start) {
+    screen_timer = timer_read32();
     qp_clear(oled_display);
-    for (int i = 0; i < 4; i++) {
-        qp_drawtext(oled_display, 0, i * 16, display_font, (char *)&screen_data_buffer[i * SCREEN_NUM_CHARS]);
+    for (int i = start, j = 0; i < start + SCREEN_NUM_LINES; i++, j++) {
+        qp_drawtext(oled_display, 0, j * 16, display_font, (char *)&screen_data_buffer[i * SCREEN_NUM_CHARS]);
     }
 }
 
@@ -100,13 +96,74 @@ void display_write_big(void) {
     }
 }
 
-void housekeeping_task_user(void) {
-    if (display_updated) {
-        qp_power(oled_display, 1);
-        display_write();
+int encoder_index = 0;
+
+void enc_counter(bool reverse) {
+    // Range from 0 to NUM_SCREENS - 1
+    if (!reverse) {
+        if (encoder_index < (NUM_SCREENS - 1)) {
+            encoder_index++;
+        }
+    } else {
+        if (encoder_index != 0) {
+            encoder_index--;
+        }
+    }
+    current_data_screen = encoder_index;
+    display_write(current_data_screen * 4);
+}
+
+void raw_hid_receive(uint8_t *data, uint8_t length) {
+    /********************************************************************************************************
+      HID report:
+
+      | - | - - - - - - - - - - - - - - - - - - | - - - - - - - - - - - - - - - |
+
+      First byte is either a 0 or a 1. 1 indicates that it is the first of 4 reports, or the first line.
+      When the first byte is a 1, the temp_data_buffer is cleared and the line number is reset.
+      This is to account for times when the communication is interrupted mid-screen, so after comms are
+      reestablished, we want to clear the temp buffer even if its not fully filled.
+
+      Second portion is SCREEN_NUM_CHARS long and is the only data that is being copied to a buffer.
+
+      Third portion is just extra padding, because the exact report rize has to be exactly 32 bytes long.
+      This portion is discarded.
+     ********************************************************************************************************/
+
+    if (data[0] == '\01') {
+        clear_buffer(temp_data_buffer);
+        current_line_number = 0;
+        text_to_buffer((char *)&data[1], temp_data_buffer, SCREEN_NUM_CHARS, current_line_number);
+    } else {
+        text_to_buffer((char *)&data[1] + sizeof(uint8_t), temp_data_buffer, SCREEN_NUM_CHARS + 1, current_line_number);
+    }
+
+    current_line_number++;
+
+    if (current_line_number >= SCREEN_NUM_LINES * NUM_SCREENS) {
+        clear_buffer(screen_data_buffer);
+        memcpy(screen_data_buffer, temp_data_buffer, sizeof(temp_data_buffer));
+        current_line_number = 0;
+        clear_buffer(temp_data_buffer);
+        display_write(current_data_screen * 4);
+        display_updated = true;
+    }
+}
+
+void check_screen_timeout(void) {
+    if (display_updated && timer_elapsed32(screen_timer) > SCREEN_TIMEOUT) {
+        qp_clear(oled_display);
+        qp_drawimage(oled_display, 0, 0, boot_splash);
+        clear_buffer(screen_data_buffer);
+        clear_buffer(temp_data_buffer);
         display_updated = false;
     }
 }
+
+void housekeeping_task_user(void) {
+    check_screen_timeout();
+}
+
 
 bool shutdown_user(bool jump_to_bootloader) {
     if (jump_to_bootloader) {
@@ -124,6 +181,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             case KC_P00:
                 tap_code(KC_P0);
                 tap_code(KC_P0);
+                return false;
+            case ENC_INC:
+                enc_counter(false);
+                return false;
+            case ENC_DEC:
+                enc_counter(true);
                 return false;
         }
     }
